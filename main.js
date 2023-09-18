79948aaf8103229b01d8b74559248d3f4b10e0fc
MikeBeloborodov <kiritohenjanai@gmail.com>
2023-09-18 12:40:23 +0400

    changed waiting logic

--- main.js
+++ main.js
@@ -5,19 +5,37 @@ class WrongWebsiteError extends Error {
   }
 }
 
-function set_zen_mode() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function wait_for_component() {
+  let chat;
+  for (let i = 0; i < 10; i++) {
+    console.log("waiting");
+    await sleep(1000);
+    try {
+      // find chat
+      chat = document.getElementsByClassName("chat-room-component")[0];
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return new Promise(resolve => {
+    resolve(chat);
+  });
+}
+
+function set_zen_mode(chat_component) {
   try {
     if (!document.URL.includes("chess.com"))
       throw new WrongWebsiteError("This website is not chess.com");
-
-    // find chat
-    const chat = document.getElementsByClassName("chat-room-component")[0];
 
     // create zen mode modal to hide chat
     const zen_modal = document.createElement('div');
 
     // add some styles
-    chat.style.position = 'relative';
+    chat_component.style.position = 'relative';
     zen_modal.style.backgroundColor = 'black';
     zen_modal.style.inset = 0;
     zen_modal.style.inlineSize = '100%';
@@ -26,10 +44,15 @@ function set_zen_mode() {
     zen_modal.style.zIndex = 2;
 
     // append modal
-    chat.appendChild(zen_modal);
+    chat_component.appendChild(zen_modal);
   } catch (error) {
     console.log(error);
   }
 }
 
-setTimeout(set_zen_mode, 1000);
+async function main() {
+  const chat_component = await wait_for_component();
+  set_zen_mode(chat_component);
+}
+
+main()
