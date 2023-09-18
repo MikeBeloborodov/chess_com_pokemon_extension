10d98f2b3b7905b5598db847498e8d49e09d4c1f
MikeBeloborodov <kiritohenjanai@gmail.com>
2023-09-18 12:13:53 +0400

    added script

---
+++ README.md
@@ -0,0 +1,2 @@
+## About
+This addon (extension) hides chat component during the game in [chess.com](chess.com)
--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-// find chat
-const chat = document.getElementsByClassName("chat-room-component")[0];
+class WrongWebsiteError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'WrongWebsiteError';
+  }
+}
 
-// create zen mode modal to hide chat
-const zen_modal = document.createElement('div');
+function set_zen_mode() {
+  try {
+    if (!document.URL.includes("chess.com"))
+      throw new WrongWebsiteError("This website is not chess.com");
 
-// add some styles
-chat.style.position = 'relative';
-zen_modal.style.backgroundColor = 'black';
-zen_modal.style.inset = 0;
-zen_modal.style.inlineSize = '100%';
-zen_modal.style.blockSize = '100%';
-zen_modal.style.position = 'absolute';
-zen_modal.style.zIndex = 2;
+    // find chat
+    const chat = document.getElementsByClassName("chat-room-component")[0];
 
-// append modal
-chat.appendChild(zen_modal);
+    // create zen mode modal to hide chat
+    const zen_modal = document.createElement('div');
+
+    // add some styles
+    chat.style.position = 'relative';
+    zen_modal.style.backgroundColor = 'black';
+    zen_modal.style.inset = 0;
+    zen_modal.style.inlineSize = '100%';
+    zen_modal.style.blockSize = '100%';
+    zen_modal.style.position = 'absolute';
+    zen_modal.style.zIndex = 2;
+
+    // append modal
+    chat.appendChild(zen_modal);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+setTimeout(set_zen_mode, 1000);
