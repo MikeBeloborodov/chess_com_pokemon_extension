5c78adff19882bc91e971d705eba002680f442d2
MikeBeloborodov <kiritohenjanai@gmail.com>
2023-09-23 16:25:44 +0400

    finished reworking

--- index.js
+++ index.js
@@ -1,96 +1,110 @@
-// Events
-const textChangeEvent = new Event("textChange");
-
 // Selectors
+const boardComponentSelector = ".board";
+const usernameComponentSelector = ".user-username-component";
 const chatRoomComponentSelector = ".chat-room-component";
-const usernameComponentSelector = ".player-top .user-tagline-username";
 const chatInputComponentSelector = ".chat-input-chat-wrapper";
-const userAvatarSelector = ".player-avatar img";
-const sidebarComponentSelector = ".sidebar-component";
-const boardComponentSelector = '.board';
+const chatInputGuestSelector = '.chat-input-guest';
 const playerTopComponentSelector = '.player-top';
 
 // Pokemon API base URL
 const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-// Functions
-function observeElementChange(selector, event) {
+// uses mutation observer to look for an element
+const waitForElement = async (selector) => {
   return new Promise((resolve) => {
-    const observer = new MutationObserver((mutations) => {
-      console.log("change! from observer");
-      document.querySelector(selector).dispatchEvent(event);
-      observer.disconnect();
-      return resolve();
-    });
-    observer.observe(document.querySelector(selector), {
-      childList: true,
-      subtree: true,
-      attributes: true,
-    });
-  });
-}
-
-function waitForElement(selector) {
-  return new Promise((resolve) => {
-    if (document.querySelector(selector)) {
+    if (document.querySelector(selector))
       return resolve(document.querySelector(selector));
-    }
-
-    const observer = new MutationObserver((mutations) => {
+    const observer = new MutationObserver((mutationsList, observer) => {
       if (document.querySelector(selector)) {
-        observer.disconnect();
         resolve(document.querySelector(selector));
+        observer.disconnect();
       }
     });
-
     observer.observe(document.body, {
       childList: true,
       subtree: true,
     });
   });
-}
+};
 
-function createZenModal() {
-  const zenModal = document.createElement("div");
-  zenModal.style.backgroundColor = "rgb(48, 46, 43)";
-  zenModal.style.inset = 0;
-  zenModal.style.inlineSize = "100%";
-  zenModal.style.blockSize = "100%";
-  zenModal.style.position = "absolute";
-  zenModal.style.zIndex = 2;
-  zenModal.style.display = "flex";
-  zenModal.style.alignItems = "center";
-  zenModal.style.justifyContent = "center";
-  return zenModal;
-}
+// this function with closure returns a function "opponentWaiter"
+// it holds prev game URL to check for a new game
+const waitForOpponent = async (usernameComponent) => {
+  return new Promise((resolve) => {
+    let prevGame = "";
+    const defaultOpponentUsername = usernameComponent.innerText;
+    return resolve(() => {
+      const re = new RegExp("https://www.chess.com/game/[0-9]*$");
+      return new Promise((resolve) => {
+        if (
+          (re.test(document.URL)) && 
+          (document.URL !== prevGame) && 
+          (document.querySelector(usernameComponentSelector).innerText !== defaultOpponentUsername)) {
+          prevGame = document.URL;
+          return resolve();
+        }
+        const observer = new MutationObserver((mutationsList, observer) => {
+          if (
+            (re.test(document.URL)) &&
+            (document.URL !== prevGame) &&
+            (document.querySelector(usernameComponentSelector).innerText !== defaultOpponentUsername)
+          ) {
+            prevGame = document.URL;
+            observer.disconnect();
+            return resolve();
+          }
+        });
+        observer.observe(document.body, {childList: true, subtree: true})
+      });
+    });
+  });
+};
 
-function createReturnChatButton(zenModal) {
-  const chatButton = document.createElement("button");
-  chatButton.style.backgroundColor = "rgb(255 255 255 / 0.08)";
-  chatButton.style.fontFamily = "Segoe UI, sans-serif";
-  chatButton.style.color = "rgb(255 255 255 / 0.72)";
-  chatButton.style.position = "relative";
-  chatButton.style.zIndex = 3;
-  chatButton.innerText = "Show chat";
-  chatButton.style.border = "none";
-  chatButton.style.fontWeight = "bold";
-  chatButton.style.fontSize = "15px";
-  chatButton.style.padding = "9px 12px";
-  chatButton.style.borderRadius = "5px";
-  chatButton.style.cursor = "pointer";
-  chatButton.addEventListener("click", (event) => {
-    zenModal.remove();
-  });
-  chatButton.addEventListener("mouseover", (event) => {
-    chatButton.style.backgroundColor = "rgb(255 255 255 / 0.16)";
-  });
-  chatButton.addEventListener("mouseleave", (event) => {
-    chatButton.style.backgroundColor = "rgb(255 255 255 / 0.08)";
-  });
-  return chatButton;
-}
+// creates a modal that covers the chat
+const createChatModal = () => {
+  const chatModal = document.createElement("div");
+  chatModal.style.backgroundColor = "rgb(48, 46, 43)";
+  chatModal.style.inset = 0;
+  chatModal.style.inlineSize = "100%";
+  chatModal.style.blockSize = "100%";
+  chatModal.style.position = "absolute";
+  chatModal.style.zIndex = 2;
+  chatModal.style.display = "flex";
+  chatModal.style.alignItems = "center";
+  chatModal.style.justifyContent = "center";
+  return chatModal;
+};
 
-function createHideChatButton(chatComponent) {
+// creates a 'return chat back' button
+const createReturnChatButton = (chatModal) => {
+  const returnChatButton = document.createElement("button");
+  returnChatButton.style.backgroundColor = "rgb(255 255 255 / 0.08)";
+  returnChatButton.style.fontFamily = "Segoe UI, sans-serif";
+  returnChatButton.style.color = "rgb(255 255 255 / 0.72)";
+  returnChatButton.style.position = "relative";
+  returnChatButton.style.zIndex = 3;
+  returnChatButton.innerText = "Show chat";
+  returnChatButton.style.border = "none";
+  returnChatButton.style.fontWeight = "bold";
+  returnChatButton.style.fontSize = "15px";
+  returnChatButton.style.padding = "9px 12px";
+  returnChatButton.style.borderRadius = "5px";
+  returnChatButton.style.cursor = "pointer";
+  returnChatButton.addEventListener("click", (event) => {
+    chatModal.remove();
+  });
+  returnChatButton.addEventListener("mouseover", (event) => {
+    returnChatButton.style.backgroundColor = "rgb(255 255 255 / 0.16)";
+  });
+  returnChatButton.addEventListener("mouseleave", (event) => {
+    returnChatButton.style.backgroundColor = "rgb(255 255 255 / 0.08)";
+  });
+  return returnChatButton;
+};
+
+// creates 'hide chat' button
+// it takes the 'close chat' function as a parameter
+const createHideChatButton = (fn) => {
   const hideChatButton = document.createElement("button");
   hideChatButton.style.backgroundColor = "rgb(255 255 255 / 0.08)";
   hideChatButton.style.fontFamily = "Segoe UI, sans-serif";
@@ -112,99 +126,138 @@ function createHideChatButton(chatComponent) {
     hideChatButton.style.backgroundColor = "rgb(255 255 255 / 0.08)";
   });
   hideChatButton.addEventListener("click", (event) => {
-    applyZenMode(chatComponent);
+    fn();
   });
   return hideChatButton;
-}
+};
 
-function addHideChatButton(chatComponent) {
-  const chatInput = document.querySelector(chatInputComponentSelector);
-  const hideChatButton = createHideChatButton(chatComponent);
-  chatInput.appendChild(hideChatButton);
-}
-
-function applyZenMode(chatComponent) {
-  const zenModal = createZenModal();
-  const returnChatButton = createReturnChatButton(zenModal);
-  chatComponent.style.position = "relative";
-  zenModal.appendChild(returnChatButton);
-  chatComponent.appendChild(zenModal);
-}
-
-async function getPokemonData(pokemonID) {
-  const response = await fetch(pokeApiUrl + pokemonID);
-  const pokeData = await response.json();
-  return pokeData;
-}
-
-async function getRandomPokemon() {
-  // Gen I (1 - 151 pokemon)
-  const random_id = Math.floor(Math.random() * 152);
-  const pokeData = await getPokemonData(random_id);
-  return pokeData;
-}
-
-async function applyPokemonMode() {
-  const pokeData = await getRandomPokemon();
-  const username = document.querySelector(usernameComponentSelector);
-  const userAvatar = document.querySelector(userAvatarSelector);
-  const observer = new MutationObserver((mutationsList, observer) => {
-    for (const mutation in mutationsList) {
-      const addedNodes = mutationsList[mutation].addedNodes;
-      if (addedNodes.length > 0)
-        if (addedNodes[0] != "Opponent") {
-          // name change
-          username.innerText = pokeData.name;
-          username.style.textTransform = "capitalize";
-          // picture change
-          const playerTopComponent = document.querySelector(playerTopComponentSelector);
-          const avatarBackground = document.createElement("div");
-          avatarBackground.style.position = "absolute";
-          avatarBackground.style.backgroundColor = "rgb(48, 46, 43)";
-          avatarBackground.style.inlineSize = "48px";
-          avatarBackground.style.blockSize = "42px";
-          avatarBackground.style.insetInlineStart = '-5px'
-          avatarBackground.style.zIndex = 2;
-          const newAvatar = document.createElement("img");
-          newAvatar.style.position = "absolute";
-          newAvatar.style.inlineSize = "44px";
-          newAvatar.style.blockSize = "42px";
-          newAvatar.style.border = 'none';
-          newAvatar.style.zIndex = 3;
-          newAvatar.style.scale = 1.9;
-          newAvatar.style.insetInlineStart = '-4px;'
-          newAvatar.src = pokeData.sprites.front_default;
-          playerTopComponent.style.position = "relative";
-          playerTopComponent.appendChild(avatarBackground);
-          playerTopComponent.appendChild(newAvatar);
-          observer.disconnect();
-        }
+// creates a modal block that closes the chat
+// it can be removed and brought back via returnChat and hideChat buttons
+const closeChat = async () => {
+  return new Promise((resolve) => {
+    const chatComponent = document.querySelector(chatRoomComponentSelector);
+    const chatInputComponent = document.querySelector(
+      chatInputComponentSelector
+    );
+    const chatModal = createChatModal();
+    const returnChatButton = createReturnChatButton(chatModal);
+    const hideChatButton = createHideChatButton(closeChat);
+    if (chatInputComponent) {
+      chatInputComponent.appendChild(hideChatButton);
+    } else {
+      const chatInputGuestComponent = document.querySelector(chatInputGuestSelector)
+      chatInputGuestComponent.append(hideChatButton)
     }
+    chatComponent.appendChild(chatModal);
+    chatModal.appendChild(returnChatButton);
+    resolve();
   });
-  observer.observe(username, {
-    attributes: true,
-    childList: true,
-    subtree: true,
-  });
+};
+
+// this function checks if chat component is null 
+// we need this to apply another chat modal to the new chat
+const waitForChatToRefresh = async() => {
+  return new Promise(resolve => {
+    const observer = new MutationObserver((mutationsList, observer) => {
+      if (document.querySelector(chatRoomComponentSelector) === null) {
+        observer.disconnect();
+        return resolve();
+      }
+    })
+    observer.observe(document.body, {childList: true, subtree: true});
+  })
 }
 
-async function main() {
-  const board = await waitForElement(boardComponentSelector);
-  const pokeMode = await applyPokemonMode();
-  const chatComponent = await waitForElement(chatRoomComponentSelector);
-  applyZenMode(chatComponent);
-  addHideChatButton(chatComponent);
-
-  const opponentNameComponent = await waitForElement(usernameComponentSelector);
-  opponentNameComponent.addEventListener("textChange", async () => {
-    console.log("text change");
-    const pokeMode = await applyPokemonMode();
-    const chatComponent = await waitForElement(chatRoomComponentSelector);
-    applyZenMode(chatComponent);
-    addHideChatButton(chatComponent);
-    observeElementChange(usernameComponentSelector, textChangeEvent);
-  });
-  observeElementChange(usernameComponentSelector, textChangeEvent);
+const getPokemonData = async(pokemonID) => {
+  return new Promise(async(resolve) => {
+    const response = await fetch(pokeApiUrl + pokemonID);
+    const pokeData = await response.json();
+    resolve(pokeData);
+  })
 }
+
+const getRandomPokemon = async() => {
+  return new Promise(async(resolve) => {
+    // Gen I (1 - 151 pokemon)
+    const random_id = Math.floor(Math.random() * 152);
+    const pokeData = await getPokemonData(random_id);
+    resolve(pokeData)
+  })
+}
+
+const transformUsername = async(pokeData) => {
+  return new Promise(resolve => {
+    const username = document.querySelector(usernameComponentSelector);
+    username.innerText = pokeData.name;
+    username.style.textTransform = 'capitalize';
+    resolve();
+  })
+}
+
+const transformAvatar = (pokeData) => {
+  return new Promise(resolve => {
+    // remove old changes
+    if (document.querySelector('.pokemon-avatar-background')){
+      console.log(document.querySelector('.pokemon-avatar-background'));
+    } 
+    if (document.querySelector('.pokemon-avatar-image')) {
+      document.querySelector('.pokemon-avatar-image').remove();
+    }
+
+    const playerTopComponent = document.querySelector(playerTopComponentSelector);
+    const avatarBackground = document.createElement("div");
+    avatarBackground.classList.add('pokemon-avatar-background');
+    avatarBackground.style.position = "absolute";
+    avatarBackground.style.backgroundColor = "rgb(48, 46, 43)";
+    avatarBackground.style.inlineSize = "48px";
+    avatarBackground.style.blockSize = "42px";
+    avatarBackground.style.insetInlineStart = '-5px'
+    avatarBackground.style.zIndex = 2;
+    const newAvatar = document.createElement("img");
+    newAvatar.classList.add('pokemon-avatar-image');
+    newAvatar.style.position = "absolute";
+    newAvatar.style.inlineSize = "44px";
+    newAvatar.style.blockSize = "42px";
+    newAvatar.style.border = 'none';
+    newAvatar.style.zIndex = 3;
+    newAvatar.style.scale = 1.9;
+    newAvatar.style.insetInlineStart = '-4px;'
+    newAvatar.src = pokeData.sprites.front_default;
+    playerTopComponent.style.position = "relative";
+    playerTopComponent.appendChild(avatarBackground);
+    playerTopComponent.appendChild(newAvatar);
+    resolve();
+  })
+}
+
+
+const applyPokemonMode = async(pokeData) => {
+  return new Promise(async(resolve) => {
+    transformUsername(pokeData)
+      .then((res) => transformAvatar(pokeData))
+      .then((res) => resolve())
+  })
+}
+
+// this recursive function waits for a new match, applies changes and then fires itself again
+const newMatch = async(opponentWaiter) => {
+  // waiting for opponent might be long, api call is faster
+  const pokeData = await getRandomPokemon();
+  opponentWaiter()
+    .then((res) => applyPokemonMode(pokeData))
+    .then((res) => waitForElement(chatRoomComponentSelector))
+    .then((res) => closeChat())
+    .then((res) => waitForChatToRefresh())
+    .then((res) => newMatch(opponentWaiter));
+};
+
+// we wait until user starts a new game and a game board appears
+const main = async () => {
+  waitForElement(boardComponentSelector)
+  .then((res) => waitForElement(usernameComponentSelector))
+  .then((usernameComponent) => waitForOpponent(usernameComponent))
+  .then((opponentWaiter) => newMatch(opponentWaiter))
+};
+
 // Entry point
 main();
